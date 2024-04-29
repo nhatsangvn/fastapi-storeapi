1502571c3de5f21836c9ad84b8433cebd8201abd
Sang Nguyen <sang@neux.io>
2024-04-29 16:23:54 +0000

    add config.py

---
+++ storeapi/tests/config.py
@@ -0,0 +1,43 @@
+from functools import lru_cache
+from typing import Optional
+
+from pydantic import BaseSettings
+
+
+class BaseConfig(BaseSettings):
+    ENV_STATE: Optional[str] = None
+
+    class Config:
+        env_file: str = ".env"
+
+
+class GlobalConfig(BaseConfig):
+    DATABASE_URL: Optional[str] = None
+    DB_FORCE_ROLL_BACK: bool = False
+
+
+class DevConfig(GlobalConfig):
+    class Config:
+        env_prefix: str = "DEV_"
+
+
+class ProdConfig(GlobalConfig):
+    class Config:
+        env_prefix: str = "PROD_"
+
+
+class TestConfig(GlobalConfig):
+    DATABASE_URL = "sqlite://test.db"
+    DB_FORCE_ROLL_BACK: True
+
+    class Config:
+        env_prefix: str = "TEST_"
+
+
+@lru_cache
+def get_config(env_state: str):
+    configs = {"dev": DevConfig, "prod": ProdConfig, "test": TestConfig}
+    return configs[env_state]()
+
+
+config = get_config(BaseConfig().ENV_STATE)
