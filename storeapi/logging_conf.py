5562a8e4d382494c8b13def41624314d1c77b076
Sang Nguyen <sang@neux.io>
2024-05-01 08:47:41 +0000

    chore: add logging-config

---
+++ storeapi/logging_conf.py
@@ -0,0 +1,53 @@
+from logging.config import dictConfig
+
+from storeapi.config import DevConfig, config
+
+
+def configure_logging() -> None:
+    dictConfig(
+        {
+            "version": 1,
+            "disable_existing_logger": False,
+            "formatters": {
+                "console": {
+                    "class": "logging.Formatter",
+                    "datefmt": "%Y-%m-%dT%H:%M:%S",
+                    "format": "%(name)s:%(lineno)d - %(message)s"
+                },
+                "file": {
+                    "class": "logging.Formatter",
+                    "datefmt": "%Y-%m-%dT%H:%M:%S",
+                    # For JsonFormatter, the format string just defines what keys are included in the log record
+                    # It's a bit clunky, but it's the way to do it for now
+                    "format": "%(asctime)s | %(msecs)03d %(levelname)-8s | %(name)s:%(lineno)d %(message)s",
+                },
+                
+            },
+            "handlers": {
+                "default": {
+                    "class": "rich.logging.RichHandler",
+                    "level": "DEBUG",
+                    "formatter": "console"
+                },
+                "rotating_file": {
+                    "class": "logging.handlers.RotatingFileHandler",
+                    "level": "DEBUG",
+                    "formatter": "file",
+                    "filename": "storeapi.log",
+                    "maxBytes": 1024 * 1024,  # 1 MB
+                    "backupCount": 5,
+                    "encoding": "utf8",
+                },
+            },
+            "loggers": {
+                "uvicorn": {"handlers": ["default", "rotating_file"], "level": "INFO"},
+                "storeapi": {
+                    "handlers": ["default", "rotating_file"],
+                    "level": "DEBUG" if isinstance(config, DevConfig) else "INFO",
+                    "propagate": False
+                },
+                "databases": {"handlers": ["default"], "level": "WARNING"},
+                "aiosqlite": {"handlers": ["default"], "level": "WARNING"},
+            }
+        }
+    )
